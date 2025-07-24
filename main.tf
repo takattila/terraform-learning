8774469588355898e3c7cd7b4e34386a88b3bd7d
takattila <takattila@gmail.com>
2025-07-24 12:53:26 +0200

    Create resource group: app-grp

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+
+  features {}
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure Client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure Client Secret"
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+}
+
+resource "azurerm_resource_group" "app_grp" {
+  name     = "app-grp"
+  location = "West Europe"
+}
