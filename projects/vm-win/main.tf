11a8625afc48e1cb77683ba7258ad61c87b06ee5
takattila <takattila@gmail.com>
2025-07-29 10:48:58 +0200

    Use subnet as resource

--- projects/vm-win/main.tf
+++ projects/vm-win/main.tf
@@ -3,22 +3,20 @@ resource "azurerm_resource_group" "app_grp" {
   location = local.location
 }
 
-data "azurerm_subnet" "SubnetA" {
-  name                 = "SubnetA"
-  virtual_network_name = azurerm_virtual_network.app_network.name
-  resource_group_name  = local.rg_name
-}
-
 resource "azurerm_virtual_network" "app_network" {
   name                = "app-network"
   location            = local.location
   resource_group_name = azurerm_resource_group.app_grp.name
   address_space       = ["10.0.0.0/16"]
+}
 
-  subnet {
-    name             = "SubnetA"
-    address_prefixes = ["10.0.1.0/24"]
-  }
+resource "azurerm_subnet" "SubnetA" {
+  name                 = "SubnetA"
+  resource_group_name  = local.rg_name
+  virtual_network_name = azurerm_virtual_network.app_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  depends_on = [azurerm_virtual_network.app_network]
 }
 
 resource "azurerm_public_ip" "app_pub_ip" {
@@ -36,13 +34,14 @@ resource "azurerm_network_interface" "app_interface" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = data.azurerm_subnet.SubnetA.id
+    subnet_id                     = azurerm_subnet.SubnetA.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.app_pub_ip.id
   }
 
   depends_on = [
     azurerm_virtual_network.app_network,
+    azurerm_subnet.SubnetA,
     azurerm_public_ip.app_pub_ip
   ]
 }
@@ -80,6 +79,7 @@ resource "azurerm_windows_virtual_machine" "app_vm" {
   }
 
   depends_on = [
+    azurerm_resource_group.app_grp,
     azurerm_network_interface.app_interface,
     azurerm_availability_set.app_set
   ]
