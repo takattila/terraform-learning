371e1606a900305029ce7d4de75be3fe78c8cf51
Takacs Attila <takattila@gmail.com>
2025-07-29 23:19:35 +0200

    Fix rg_name in modules\vm-win\variables.tf

--- modules/vm-win/variables.tf
+++ modules/vm-win/variables.tf
@@ -1,7 +1,7 @@
 locals {
   raw_ts       = timestamp()
   short_ts     = formatdate("YYMMDDhhmm", local.raw_ts)
-  rg_name      = "rg-vm-infra"
+  rg_name      = "rg-vm-win"
   location     = "West Europe"
   storage_name = "tformstore${local.short_ts}"
 }
