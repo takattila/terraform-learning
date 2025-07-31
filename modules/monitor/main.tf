9527021559bc079f5852f80ad85e5cf4604f0084
Takacs Attila <takattila@gmail.com>
2025-07-31 22:54:12 +0200

    Monitor Allow-80 rule

--- modules/monitor/main.tf
+++ modules/monitor/main.tf
@@ -97,6 +97,18 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name                       = "Allow-80"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   depends_on = [azurerm_resource_group.rg]
 }
 
